4ae96f719fa3d2a8ab44b0b8443fd59962881b3a
riddhisht <0001212ab@gmail.com>
2024-04-09 19:52:28 -0400

    Updated model and requirements file

--- model.py
+++ model.py
@@ -75,3 +75,84 @@ test_loader = torch.utils.data.DataLoader(test_set, batch_size=BATCH_SIZE,
                                               shuffle=True, drop_last=False)
 for idx, (data,label) in enumerate(test_loader):
     print(idx, data.shape, label.shape)
+
+
+
+
+##################################################
+class TrialCNN(nn.Module):
+    def __init__(self):
+        super(TrialCNN, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=16, kernel_size=3,  padding=1),
+            nn.BatchNorm2d(16),
+            #nn.ReLU(),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2,  padding=1)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            #nn.ReLU(),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.layer3 = nn.Sequential(
+        nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+        nn.BatchNorm2d(32),
+        #nn.ReLU(),
+        nn.LeakyReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.layer4 = nn.Sequential(
+        nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+        nn.BatchNorm2d(32),
+        #nn.ReLU(),
+        nn.LeakyReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.layer5 = nn.Sequential(
+        nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3, padding=1),
+        nn.BatchNorm2d(32),
+        #nn.ReLU(),
+        nn.LeakyReLU(),
+        nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.fc1 = nn.Sequential(
+            # nn.Linear(in_features=882048, out_features=100),
+            nn.Linear(in_features=8832, out_features=1000),
+            #nn.ReLU()
+            nn.LeakyReLU(),
+        )
+        self.drop = nn.Dropout(0.25)
+
+        self.fc2 = nn.Sequential(
+            nn.Linear(in_features=1000, out_features=100),
+            #nn.ReLU()
+            nn.LeakyReLU(),
+        )
+        self.drop = nn.Dropout(0.25)
+
+        self.fc3 = nn.Linear(in_features=100, out_features=2)
+
+    def forward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.drop(out)
+        out = self.fc2(out)
+        out = self.drop(out)
+        out = self.fc3(out)
+
+        return out
+#################################################################
